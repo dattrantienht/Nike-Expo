c9909424b963dde8f44eb0dad641fefd8ea44558
Trần Tiến Đạt <dattrantienht@gmail.com>
2021-12-10 17:20:19 +0700

    add basic drawer

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Home from './components/Home';
+import Login from './components/Login';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Home/>
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Login" component={Login} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
