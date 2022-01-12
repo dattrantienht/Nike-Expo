76a97c3ec109cc2f9fa29352b80553f03b24e22c
Trần Tiến Đạt <dattrantienht@gmail.com>
2022-01-12 20:55:06 +0700

    Update Shop.js

--- components/Shop.js
+++ components/Shop.js
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import { StatusBar, StyleSheet, Text, View, FlatList } from 'react-native';
+import { StatusBar, StyleSheet, Image, Text, View, FlatList } from 'react-native';
 import { useTheme } from '@react-navigation/native';
 
 let listProduct;
+let imageUriTest = 'https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/f0c6bdc2-b8fe-45f7-a76d-ae1bbf30e156/air-jordan-4-crimson-ct8527-016-release-date.jpg';
+
 
 async function getListProduct() {
   try {
@@ -18,16 +20,18 @@ async function getListProduct() {
 const Product = ({ name, price, image }) => (
   <View style={styles.item}>
     <Text>{name}</Text>
+    <Image
+      style={styles.productImage}
+      source={{uri: image}}/>
     <Text>{price}</Text>
   </View>
 );
 
 export default function Shop() {
   const { colors } = useTheme();
-
   const [items, setItems] = useState([]);
   const renderItem = ({ item }) => (
-    <Product name={item.name} price={item.price} />
+    <Product name={item.name} price={item.price} image={item.image} />
   );
 
   useEffect( async ()=>{
@@ -57,10 +61,14 @@ export default function Shop() {
     },
     item: {
       width: 160,
-      height: 180,
+      height: 200,
       backgroundColor: 'white',
       padding: 10,
       marginVertical: 8,
       marginHorizontal: 16,
+    },
+    productImage: {
+      width: 120,
+      height: 130,
     },
   });
