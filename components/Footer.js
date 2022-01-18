722b78a16dff217b725123ecd5f3fc527313a71c
LamNgo <74997305+LamNgoSKet@users.noreply.github.com>
2022-01-18 19:45:47 +0700

    Update Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Linking, TouchableOpacity } from 'react-native';
+import { StyleSheet,Text, View, Linking, TouchableOpacity } from 'react-native';
 import {SocialIcon} from 'react-native-elements';
 class Footer extends Component{
 	render (){
 		return (
-			<View style={{justifyContent:'center', alignItems:'center'}}>
+			<View style={{justifyContent:'center', alignItems:'center',paddingTop:30}}>
 				<TouchableOpacity onPress={()=>Linking.openURL('https://www.facebook.com/Ninja-In-PyJamas-102421947783975')}>
           			<Text style={{ fontSize:17,color:'#3E9CFF' }}> Find a Store </Text>
  				</TouchableOpacity>
-				 <TouchableOpacity onPress={()=>Linking.openURL('https://www.facebook.com/Ninja-In-PyJamas-102421947783975')}>
-          			<Text style={{ fontSize:17,color:'#3E9CFF' }}>                   * Become A Member * </Text>
+				 <TouchableOpacity onPress={()=>Linking.openURL('https://www.nike.com/vn/register')}>
+          			<Text style={{ fontSize:17,color:'#3E9CFF',padding:3 }}> Become A Member  </Text>
  				</TouchableOpacity>
-			  <View style={{flexDirection: 'row'}}>
-                <SocialIcon
+				 <Text style={{ fontSize:17,color:'#3E9CFF',padding:3 }}> Sign Up For Email  </Text>
+				 <Text style={{ fontSize:17,color:'#3E9CFF',padding:3 }}> Send Us FeedBack  </Text>
+			  <View style={{flexDirection: 'row',}}>
+                <SocialIcon 
                   type="facebook"
                   onPress={() => Linking.openURL('https://www.facebook.com/Ninja-In-PyJamas-102421947783975')}>
 				  </SocialIcon>
